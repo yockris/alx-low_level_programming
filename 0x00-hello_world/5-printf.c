fb56ccc9e5e4e7642960c25b7ceedef1c52ec730
yockris <davejou.cj@gmail.com>
2023-09-07 17:22:01 -0700

    5 printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This program will display
+ * "with proper grammer, but the outcome is a piece of art,
+ *
+ * Return: (0) success
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
