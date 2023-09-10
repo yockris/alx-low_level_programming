c9ab36206dac5df15a0ccaee3ff9a002a85164fd
yockris <davejou.cj@gmail.com>
2023-09-10 08:50:53 -0700

    2 print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the program will print alphabets in lover cases
+ * followed by a new line
+ *
+ * Return: (0) success
+ */
+int main(void)
+{
+	char letter = 'a';
+	while (letter <= 'z'){
+		putchar(letter);
+			letter++;
+		}
+	putchar('\n');
+	return (0);
+}
