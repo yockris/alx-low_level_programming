59d7140454fb58ae6ed3d48970d3bc929376b71a
yockris <davejou.cj@gmail.com>
2023-09-10 16:23:43 -0700

    3 print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,17 @@
  *
  * Return: (0) success
  */
+
 int main(void)
+
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
