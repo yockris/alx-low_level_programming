76467ba51ad2e2e6120b22af4e90f1ea06f61446
yockris <davejou.cj@gmail.com>
2023-09-10 09:44:27 -0700

    2 print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,14 @@
 int main(void)
 {
 	char letter = 'a';
-	while (letter <= 'z') 
-	{
-		putchar(letter);
-		letter++;
-		}
+	while (letter <= 'z')
+	
+{
+
+	putchar(letter);
+	letter++;
+
+}
 	putchar('\n');
 	return (0);
 }
