eca4e66e19e6f17c2b822f6a6491cdf87f436c0c
yockris <davejou.cj@gmail.com>
2023-09-11 06:16:04 -0700

    6 print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program
+ *
+ * Return: (0) success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar(a + '0');
+	putchar('\n');
+	return (0);
+}
