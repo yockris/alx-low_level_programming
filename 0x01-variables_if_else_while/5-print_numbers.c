b27dc7d0720f5524e7d0cf3dea36d58ee3728760
yockris <davejou.cj@gmail.com>
2023-09-10 17:30:50 -0700

    5 print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the program will display 0-9
+ *
+ * Return: (0) success
+ */
+
+int main(void)
+
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+
+	printf("\n");
+
+	return (0);
+}
