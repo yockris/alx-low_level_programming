346c64db1ab95f360c5bbf79455e29daecd7954d
yockris <davejou.cj@gmail.com>
2023-09-13 15:03:35 -0700

    0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main - the program will print _putchar
+ * followed by a new line
+ *
+ * Return: (0) success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+return (0);
+}
