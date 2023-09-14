536b8d94422c390af05d4e1706b1af048a869b1e
yockris <davejou.cj@gmail.com>
2023-09-14 16:50:30 -0700

    5 more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print numbers from 0 to 14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+				_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
+
